9070c5853ec230e2e644dfbc6fca8403f35975f1
alidanish783 <142690005+alidanish783@users.noreply.github.com>
2024-07-14 20:56:41 +0530

    script.js

--- script.js
+++ script.js
@@ -1,56 +1,57 @@
-let box=document.querySelectorAll(".box");
-let resetbtn=document.querySelector(".reset");
-let turno=true;
-let newgamebtn=document.querySelector("#newgame");
-let msgcontainer=document.querySelector(".msg-container");
-let msg=document.querySelector("#msg");
+document.addEventListener('DOMContentLoaded', function () {
+    const cells = document.querySelectorAll('.cell');
+    const resetButton = document.getElementById('reset-button');
+    let currentPlayer = 'X';
+    let board = ['', '', '', '', '', '', '', '', ''];
 
-const winpattern=[
-    [0,1,2],
-    [0.3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8]
-    ];
-    box.forEach((box) => {
-    box.addEventListener("click",() => {
-        console.log("Box was clicked");
-        if(turno)
-            {
-                box.innerText="O";
-                turno=false;
-            }
-            else{
-                box.innerText="X";
-                turno=true;
-            }
-            box.disabled=true; //disables a button if click ones
-            checkWinner();
+    cells.forEach(cell => {
+        cell.addEventListener('click', handleCellClick);
     });
-});
 
-const showWinner=(pos)=>{
-msg.innerText='Winner is '+pos1;
-msgcontainer.classList.remove("hide");
+    resetButton.addEventListener('click', resetGame);
 
-}
-    let checkWinner =() => {
-        for(let pattern of winpattern ){
+    function handleCellClick(event) {
+        const cell = event.target;
+        const index = cell.getAttribute('data-index');
 
-            let pos1 = box[pattern[0]].innerText; 
-            let pos2=box[pattern[1]].innerText;
-            let pos3=box[pattern[2]].innerText;
+        if (board[index] === '') {
+            board[index] = currentPlayer;
+            cell.textContent = currentPlayer;
 
-            if(pos1!="" && pos2!="" && pos3!="")
-            {
-                if(pos1===pos2 && pos2===pos3)
-                {
-                    console.log("Winner",pos1);
-                    showWinner(pos1);
-                }
+            if (checkWin(currentPlayer)) {
+                alert(`${currentPlayer} wins!`);
+                resetGame();
+            } else if (board.every(cell => cell !== '')) {
+                alert('It\'s a tie!');
+                resetGame();
+            } else {
+                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
             }
         }
-    };
+    }
+
+    function checkWin(player) {
+        const winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        return winningCombinations.some(combination => {
+            return combination.every(index => board[index] === player);
+        });
+    }
+
+    function resetGame() {
+        board = ['', '', '', '', '', '', '', '', ''];
+        cells.forEach(cell => {
+            cell.textContent = '';
+        });
+        currentPlayer = 'X';
+    }
+});
